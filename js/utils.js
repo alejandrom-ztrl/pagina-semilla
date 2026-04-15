c1d26f1468fd9d76da9e7fb4c17644c02a5486ed
alejandrom-ztrl <alejandrom1503@hotmail.com>
2026-04-15 18:27:34 +0200

    ASFSADFAFAFAF

--- js/utils.js
+++ js/utils.js
@@ -34,11 +34,10 @@ function generarEtiquetaCosecha() {
     const cortadoFormat = `${cortadoParts[2]}/${cortadoParts[1]}`;
 
     const pNombre = l.plantaNombre.trim().toUpperCase();
-    let imgName = pNombre;
-    if (imgName === "GUISANTE" || imgName === "GUISANTES") imgName = "GUISANTE FOXY";
     
-    document.getElementById('etiq-bg-img').src = `img/${imgName.replace(/ /g, '%20')}.png`;
+    document.getElementById('etiq-bg-img').src = `img/PLANTILLA.png`;
 
+    document.getElementById('txt-planta').innerText = pNombre;
     document.getElementById('txt-cliente').innerText = clienteEditado;
     document.getElementById('txt-lote').innerText = loteCod;
     document.getElementById('txt-cortado').innerText = cortadoFormat;
