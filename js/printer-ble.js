af84203c5331cb50a3566a09454b0fcb8e7a47f8
alejandrom-ztrl <alejandrom1503@hotmail.com>
2026-06-04 13:35:48 +0200

    Ajustar escala a 0.93 (71.5mm) para dejar margen fisico de sensor y evitar saltos de etiquetas

--- js/printer-ble.js
+++ js/printer-ble.js
@@ -216,8 +216,8 @@ const PRINTER_BLE = {
             // 2. Escalar al ancho de la M110S (384px) - ROTADO (Vertical)
             const targetWidth = this.PRINT_WIDTH_PX; // 384
             const scale = targetWidth / sourceCanvas.height; 
-            // Escalamos a 76mm de largo (608px en total) para aprovechar la etiqueta de 80mm
-            const targetHeight = Math.round(sourceCanvas.width * scale * 0.99);
+            // Escalamos a 71.5mm de largo (572px en total) para aprovechar la etiqueta de 80mm sin desbordar con los márgenes físicos
+            const targetHeight = Math.round(sourceCanvas.width * scale * 0.93);
 
             const printCanvas = document.createElement('canvas');
             printCanvas.width = targetWidth;
--- js/printer-serial.js
+++ js/printer-serial.js
@@ -98,8 +98,8 @@ const PRINTER_SERIAL = {
             // 2. Escalar al ancho de la M110S (384px) - ROTADO (Vertical)
             const targetWidth = this.PRINT_WIDTH_PX;
             const scale = targetWidth / sourceCanvas.height;
-            // Escalamos a 76mm de largo (608px en total) para aprovechar la etiqueta de 80mm
-            const targetHeight = Math.round(sourceCanvas.width * scale * 0.99);
+            // Escalamos a 71.5mm de largo (572px en total) para aprovechar la etiqueta de 80mm sin desbordar con los márgenes físicos
+            const targetHeight = Math.round(sourceCanvas.width * scale * 0.93);
 
             const printCanvas = document.createElement('canvas');
             printCanvas.width = targetWidth;
